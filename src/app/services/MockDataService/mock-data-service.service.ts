559e101dbcff87a41908544c95b442c3ba463da7
Gabriel Santos <gabrieljbs@gmail.com>
2024-08-28 12:17:34 -0300

    retirado dados não usados

--- src/app/services/MockDataService/mock-data-service.service.ts
+++ src/app/services/MockDataService/mock-data-service.service.ts
@@ -17,28 +17,6 @@ interface Country {
   path: string;
 }
 
-interface Industrie {
-  name: string;
-  path: string;
-  industrie: Industries[];
-}
-
-interface Industries {
-  name: string;
-  path: string;
-}
-
-interface Expertise {
-  name: string;
-  path: string;
-  expertise: Expertises[];
-}
-
-interface Expertises {
-  name: string;
-  path: string;
-}
-
 
 @Injectable({
   providedIn: 'root',
@@ -73,44 +51,10 @@ export class MockDataServiceService {
     ],
   };
 
-  public industries: Industrie = {
-    name: 'Industrie',
-    path: 'industrie',
-    industrie: [
-      {
-        name: 'Energy',
-        path: 'energy',
-      },
-      {
-        name: 'Financial institutions',
-        path: 'financial-institutions',
-      },
-    ],
-  };
-
-  public expertise: Expertise = {
-    name: 'Expertise',
-    path: 'expertise',
-    expertise: [
-      {
-        name: 'Corporate and M&A',
-        path: 'corporate-and-mea',
-      },
-      {
-        name: 'Disputes',
-        path: 'disputes',
-      },
-    ],
-  };
 
   getDataGlobalCoverage() {
     return this.globalCoverage;
   }
-  getDataIndustrie() {
-    return this.industries;
-  }
-  getDataExpertise() {
-    return this.expertise;
-  }
+
 
 }
