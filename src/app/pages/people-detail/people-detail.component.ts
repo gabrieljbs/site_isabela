b4405e473798988def151ccc495ed50e6cc09a79
Gabriel Santos <gabrieljbs@gmail.com>
2024-10-24 15:19:41 -0300

    Modificação para receber dados de socios

--- src/app/pages/people-detail/people-detail.component.ts
+++ src/app/pages/people-detail/people-detail.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../../services/dataServices/data.service';
 
 @Component({
   selector: 'app-people-detail',
   standalone: true,
   imports: [],
   templateUrl: './people-detail.component.html',
-  styleUrl: './people-detail.component.scss'
+  styleUrl: './people-detail.component.scss',
 })
 export class PeopleDetailComponent {
+  pessoa: any[] = [];
+  dados: any[] = [];
+  constructor(private router: Router, private dadosService: DataService) {
+    const meuDado = this.router.getCurrentNavigation()?.extras.state?.['id'];
+    console.log(meuDado);
 
+    this.dadosService.carregarDados().subscribe(
+      (dados) => {
+        this.dados = dados;
+          
+           this.pessoa = this.dados.filter(dado => dado.id === meuDado); 
+
+        console.log(this.dados);
+      },
+      (error) => {
+        console.error('Erro ao carregar os dados', error);
+      }
+    );
+  }
 }
