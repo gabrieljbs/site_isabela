45e978f00476d9f034edc20d5202d37a7f85e834
Gabriel Santos <gabrieljbs@gmail.com>
2024-10-24 15:19:58 -0300

    Modificação para receber usar httpClient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,23 @@ import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LogoLeftComponent } from './components/logo-left/logo-left.component';
 import { NewHeaderComponent } from './components/new-header/new-header.component';
-
+import { HttpClientModule } from '@angular/common/http'; 
+import { DataService } from './services/dataServices/data.service'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,FooterComponent,LogoLeftComponent, NewHeaderComponent],
+  imports: [HttpClientModule,RouterOutlet, HeaderComponent,FooterComponent,LogoLeftComponent, NewHeaderComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'front_tample';
+
+  constructor() {}
+
+  ngOnInit() {
+   
+
+  }
+  
 }
