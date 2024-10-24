4f6d75045659122667411b483c59f2de7bee1c63
Gabriel Santos <gabrieljbs@gmail.com>
2024-10-24 08:38:24 -0300

    a

---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBfgIxvT6_30cwbNwZ4jqNsKUnqZmz9PdU",
+        authDomain: "site-costa-rodrigues.firebaseapp.com",
+        projectId: "site-costa-rodrigues",
+        storageBucket: "site-costa-rodrigues.appspot.com",
+        messagingSenderId: "268613199298",
+        appId: "1:268613199298:web:2a5dc77bd523fb8488e25b"
+    }
+  };
