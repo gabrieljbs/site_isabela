b0f8bc4d2160b05862b04fd0eee4a5a3ad4575a6
Gabriel Santos <gabrieljbs@gmail.com>
2024-10-29 10:20:25 -0300

    Ajustando a requisição para leitura dos dados

--- src/app/services/pessoalServices/pessoal-service.service.ts
+++ src/app/services/pessoalServices/pessoal-service.service.ts
@@ -4,14 +4,19 @@ import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
-export class DataService {
+export class PessoalService {
+  private jsonURL = 'assets/dados.json'; 
   private apiUrl = 'http://localhost:3000/api'
 
   constructor(private http: HttpClient) {}
 
-  async getData(){
+  async list(){
     const res = await this.http.get(`${this.apiUrl}/pessoal`);
     return res
+  }
+
+ getById(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/pessoal/${id}`);
   }
 
 }
