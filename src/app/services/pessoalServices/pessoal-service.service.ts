395b1681fa1db12eab484311e4af115a6b82e011
Gabriel Santos <gabrieljbs@gmail.com>
2024-10-30 19:25:42 -0300

    prod backend

--- src/app/services/pessoalServices/pessoal-service.service.ts
+++ src/app/services/pessoalServices/pessoal-service.service.ts
@@ -6,7 +6,8 @@ import { Observable } from 'rxjs';
 })
 export class PessoalService {
   private jsonURL = 'assets/dados.json'; 
-  private apiUrl = 'http://localhost:3000/api'
+  // private apiUrl = 'http://localhost:3000/api'
+  private apiUrl = 'http://https://backend-costa-rodrigues-85tyzw315-gabriels-projects-368d0f0a.vercel.app//api'
 
   constructor(private http: HttpClient) {}
 
