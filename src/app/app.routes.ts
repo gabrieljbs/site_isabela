f107991af99383f76f9afad4bbbe3fc89e3e448d
Gabriel Santos <gabrieljbs@gmail.com>
2024-11-01 08:48:38 -0300

    Deletado pagina não utilizada

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,24 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { PeopleSearchComponent } from './pages/people-search/people-search.component';
 import { CareersComponent } from './pages/careers/careers.component';
-import { NewsroomComponent } from './pages/newsroom/newsroom.component';
 import { OfficeComponent } from './pages/office/office.component';
 import { PerformanceComponent } from './pages/performance/performance.component';
 import { PeopleDetailComponent } from './pages/people-detail/people-detail.component';
 import { LifeSciencesComponent } from './pages/life-sciences/life-sciences.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { InsightsComponent } from './pages/insights/insights.component';
+import { AtuacaoComponent } from './pages/atuacao/atuacao.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo:'/home', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'people-search', component: PeopleSearchComponent },
   { path: 'careers', component: CareersComponent },
-  { path: 'newsroom', component: NewsroomComponent },
   { path: 'office', component: OfficeComponent },
   { path: 'performance', component: PerformanceComponent },
-  { path: 'people-detail',component:PeopleDetailComponent},
-  { path: 'life-sciences', component: LifeSciencesComponent},
-  { path: 'about-us', component: AboutUsComponent},
-  {path:'insights', component:InsightsComponent}
+  { path: 'people-detail', component: PeopleDetailComponent },
+  { path: 'life-sciences', component: LifeSciencesComponent },
+  { path: 'about-us', component: AboutUsComponent },
+  { path: 'insights', component: InsightsComponent },
+  { path: 'atuacao', component:AtuacaoComponent },
 ];
